63b38ea1acedd69050f05059d9e63591e64e10dd
ajithcravi <craviajith@gmail.com>
2019-12-10 22:24:51 +0530

    iframe

--- app.js
+++ app.js
@@ -1,30 +1,83 @@
-class Taxi {
-  constructor(
-    basefareDetails,
-    next15KmsFare,
-    additionalKmFare,
-    perksBeyondLimit
-  ) {
-    this.basefare = basefareDetails.basefare;
-    this.basefareDistance = basefareDetails.basefaredistance;
-    this.next15KmsFare = next15KmsFare;
-    this.additionalKmFare = additionalKmFare;
-    this.perksLimitDistance = perksBeyondLimit.limit;
-    this.perksFare = perksBeyondLimit.fare;
-  }
+// // This class contains the basefare and its respective distance
+// class FareSlab {
+//   constructor(distance, fare) {
+//     this.distance = distance;
+//     this.fare = fare;
+//   }
+// }
 
-  calculateFare = distance => {
-    //If there is perks above a particular distance and the input distance is greater than that
-    if (this.perksLimitDistance && distance >= this.perksLimitDistance) {
-      return distance * this.perksFare;
-    }
-  };
-}
+// class Taxi {
+//   constructor(name
+//   ) {
+//     this.name = name}
 
-//Creating the mini type taxi
-let mini = new Taxi({ basefare: 50, basefaredistance: 3 }, 10, 8, {
-  limit: 75,
-  fare: 8
-});
+//     taxiSlab = this.name => if (this.name === 'mini'){
+//       let basefare = new FareSlab(3,50);
+//       let nextFfare = new FareSlab(15,10);
+//       let additionalFare = new FareSlab(1,8);
+//       let limitFare = new FareSlab(75,8);
+//     }
+//     else if (this.name === 'sedan'){
+//       let basefare = new FareSlab(5,80);
+//       let nextFfare = new FareSlab(15, 12);
+//       let additionalFare = new FareSlab(1, 10);
+//       let limitFare = new FareSlab(100, 10);
+//     }
+//     else if (this.name === 'suv'){
+//       let basefare = new FareSlab(5, 100);
+//       let nextFfare = new FareSlab(15, 15);
+//       let additionalFare = new FareSlab(1, 12);
+//       let limitFare = new FareSlab(0, 0);
+//   }
 
-console.log(mini.calculateFare(100));
+//   calculateFare = distance => {
+//     //If there is perks above a particular distance and the input distance is greater than that
+//     if (this.perksLimitDistance && distance >= this.perksLimitDistance) {
+//       return distance * this.perksFare;
+//     } else {
+//       if (distance <= this.basefareDistance) {
+//         return this.basefare;
+//       } else if (distance <= this.basefareDistance + this.fareLimitDistance) {
+//         return (
+//           this.basefare + (distance - this.basefareDistance) * this.fareLimit
+//         );
+//       } else {
+//         console.log(this.basefareDistance, this.fareLimitDistance);
+//         return (
+//           this.basefare +
+//           this.fareLimitDistance * this.fareLimit +
+//           (distance - this.basefareDistance - this.fareLimitDistance) *
+//             this.additionalKmFare
+//         );
+//       }
+//     }
+//   };
+// }
+
+// console.log(mini.calculateFare(10));
+// console.log(sedan.calculateFare(10));
+// console.log(suv.calculateFare(10));
+
+// console.log(mini.calculateFare(20));
+// console.log(sedan.calculateFare(20));
+// console.log(suv.calculateFare(20));
+
+// console.log(mini.calculateFare(50));
+// console.log(sedan.calculateFare(50));
+// console.log(suv.calculateFare(50));
+
+// // class FareSlab {
+// //   constructor(type, distance, fare) {
+// //     this.type = type;
+// //     this.distance = distance;
+// //     this.fare = fare;
+// //   }
+
+// //   calculateFare = () => {
+// //     if (type === "basefare") {
+// //       return this.fare;
+// //     } else {
+// //       return this.distance * this.fare;
+// //     }
+// //   };
+// // }
