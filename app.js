3df2a19aad475220c8cec22bace3d6fdc06fb1c6
ajithcravi <craviajith@gmail.com>
2019-12-10 10:49:39 +0530

    beyondlimit working fine

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-class taxi {
+class Taxi {
   constructor(
     basefareDetails,
     next15KmsFare,
@@ -6,10 +6,25 @@ class taxi {
     perksBeyondLimit
   ) {
     this.basefare = basefareDetails.basefare;
-    this.basefareDistance = basefareDetails.distance;
+    this.basefareDistance = basefareDetails.basefaredistance;
     this.next15KmsFare = next15KmsFare;
     this.additionalKmFare = additionalKmFare;
     this.perksLimitDistance = perksBeyondLimit.limit;
     this.perksFare = perksBeyondLimit.fare;
   }
+
+  calculateFare = distance => {
+    //If there is perks above a particular distance and the input distance is greater than that
+    if (this.perksLimitDistance && distance >= this.perksLimitDistance) {
+      return distance * this.perksFare;
+    }
+  };
 }
+
+//Creating the mini type taxi
+let mini = new Taxi({ basefare: 50, basefaredistance: 3 }, 10, 8, {
+  limit: 75,
+  fare: 8
+});
+
+console.log(mini.calculateFare(100));
